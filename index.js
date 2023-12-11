cf15015704e92c2a7494f014ac8cba9b6705a64d
x23756575 <samoreillyashbourne@icloud.com>
2023-12-11 14:00:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.js
@@ -0,0 +1,73 @@
+
+function briefInfo1(x) {
+    text1.style.display = 'block';
+    figcaption1.style.display = 'none';
+}
+function backToNormal1(x) {
+    x.style.scale = "100%";
+    text1.style.display = 'none';
+    figcaption1.style.display = 'block';
+}
+function briefInfo2(x) {
+    text2.style.display = 'block';
+    figcaption2.style.display = 'none';
+}
+function backToNormal2(x) {
+    x.style.scale = "100%";
+    text2.style.display = 'none';
+    figcaption2.style.display = 'block';
+}
+function briefInfo3(x) {
+    text3.style.display = 'block';
+    figcaption3.style.display = 'none';
+}
+function backToNormal3(x) {
+    x.style.scale = "100%";
+    text3.style.display = 'none';
+    figcaption3.style.display = 'block';
+}
+function briefInfo4(x) {
+    text4.style.display = 'block';
+    figcaption4.style.display = 'none';
+}
+function backToNormal4(x) {
+    x.style.scale = "100%";
+    text4.style.display = 'none';
+    figcaption4.style.display = 'block';
+}
+function briefInfo5(x) {
+    text5.style.display = 'block';
+    figcaption5.style.display = 'none';
+}
+function backToNormal5(x) {
+    x.style.scale = "100%";
+    text5.style.display = 'none';
+    figcaption5.style.display = 'block';
+}
+function briefInfo6(x) {
+    text6.style.display = 'block';
+    figcaption6.style.display = 'none';
+}
+function backToNormal6(x) {
+    x.style.scale = "100%";
+    text6.style.display = 'none';
+    figcaption6.style.display = 'block';
+}
+function briefInfo7(x) {
+    text7.style.display = 'block';
+    figcaption7.style.display = 'none';
+}
+function backToNormal7(x) {
+    x.style.scale = "100%";
+    text7.style.display = 'none';
+    figcaption7.style.display = 'block';
+}
+function briefInfo8(x) {
+    text8.style.display = 'block';
+    figcaption8.style.display = 'none';
+}
+function backToNormal8(x) {
+    x.style.scale = "100%";
+    text8.style.display = 'none';
+    figcaption8.style.display = 'block';
+}
